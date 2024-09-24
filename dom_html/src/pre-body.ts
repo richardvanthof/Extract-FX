55860c3be5937ddb311f183865a39bfb283df2bf
Richard van 't Hof <hello@therichard.space>
2024-09-24 20:21:18 +0200

    Added new frontend functions - set up static video track and effect track lists. - added exclusion adding functionality - minor styling imporvements - WIP: passing trackIndex and exclusions to csInterface()

--- dom_html/src/pre-body.ts
+++ dom_html/src/pre-body.ts
@@ -19,9 +19,10 @@ const debug = false
 
 function handleCallback(data) {
     if(data) showLoadingScreen(false)
-    if(data !== true) {
-        alert(data);
-        console.error(data)
+    if (data !== true) {
+            alert(data.message); // Handle other types of data
+            console.error(data, data.message);
+        }
     }
 }
 
@@ -46,13 +47,30 @@ $( document ).ready(function() {
     
     $("#extract-btn").on("click", function(e){
         e.preventDefault(); 
-        // get target video track
-        const targetTrack = document.querySelector('#track').value
-
-        var csInterface = new CSInterface();
-        //alert('button clicked')
-        showLoadingScreen(true)
-        csInterface.evalScript(`$._PPP_.copyClipEffectsToAdjustmentLayers()`, handleCallback);
+        try {
+            // get target video track
+            const targetTrack = document.querySelector('#track').value;
+            const effectExclusionsElems = document.querySelectorAll('.exclusion-dropdown');
+            
+            // console.log(effectExclusionsElems)
+            let effectExclusions:string[] = [];
+            for(let fx of effectExclusionsElems) effectExclusions.push(fx.value)
+            
+            var csInterface = new CSInterface();
+            
+            if(!targetTrack) throw 'TargetTrack is undefined'
+            if(!effectExclusions) throw 'Effect exclusions are undefined.'
+            
+            showLoadingScreen(true)
+            csInterface.evalScript(`$._PPP_.copyClipEffectsToAdjustmentLayers(${targetTrack}, ${effectExclusions})`, handleCallback);
+        } catch(err) {
+            alert(err)
+            throw new Error(err)
+        }
+        
+        
+            //console.log(effectExclusions)
+       
     });
 
     $("#help-btn").on("click", function(e){
@@ -67,6 +85,89 @@ $( document ).ready(function() {
         }
         csInterface.openURLInDefaultBrowser(path);	
     });
+
+    const exclusionsContainer = document.querySelector('.exclusions-container');
+    const addExclusionBtn = document.querySelector('#add-exclusion-btn');
+    const removeExclusionBtn = document.querySelectorAll('.remove-exclusion')
+    const removeAllExclusions = document.querySelector('#remove-all-exclusions-btn');
+
+    let exclCounter = 0;
+
+    function updateCounter(amount) {
+        exclCounter += amount;
+        console.log(exclCounter);
+        const header = document.querySelector('#exclusion-header');
+        if (exclCounter = 0) {
+            header.innerHTML = `Exclude effects (${counter})`
+        } else {
+            header.innerHTML = `Exclude effects`;
+        }
+    }
+
+    function togglePlaceholder() {
+        if (exclusionsContainer.children.length == 0) {
+            exclusionsContainer.innerHTML = '<div class="exclusions-placeholder"><h5>Add exclusion by pressing the +-button.</h5><p style="margin-top: 1em; color: rgba(255,255,255,0.4)">Tip: we recommended to exclude Warp Stabilizer and Lumetri Color.</p></div>';
+        }
+    }
+
+
+    removeAllExclusions.addEventListener('click', e => {
+
+            e.preventDefault();
+            exclusionsContainer.innerHTML = '';
+            togglePlaceholder();
+        
+    })
+
+    addExclusionBtn.addEventListener('click', e => {
+        let effectsList = "Color Balance (RGB),Color Replace,Gamma Correction,Extract,Color Pass,Lens Distortion,Solarize,Convolution Kernel,Levels,MPEG Source Settings,Turbulent Displace,Gradient Wipe,Ultra Key,Vector Motion,Linear Wipe,Twirl,Alpha Adjust,VR Plane to Sphere,Radial Wipe,Change to Color,RGB Curves,Echo,Ramp,Sony Raw Source Settings,Non Red Key,Lighting Effects,Eight-Point Garbage Matte,Color Balance (HLS),Vertical Flip,Broadcast Colors,Wave Warp,Venetian Blinds,Lumetri Color,Bevel Edges,Auto Levels,VR Blur,PRORES RAW Source Settings,Equalize,Shape,Checkerboard,Block Dissolve,Unsharp Mask,VR Glow,Median (Legacy),Set Matte,Set Matte,Auto Color,Spherize,Directional Blur,Difference Matte,Mirror,Noise,Noise,Roughen Edges,Bevel Alpha,Auto Contrast,Luma Corrector,Magnify,Compound Blur,VR Rotate Sphere,Pixel Motion Blur,Rolling Shutter Repair,Timewarp,Three-Way Color Corrector,Clip Name,Arithmetic,Paint Bucket,Fast Blur,Reduce Interlace Flicker,Sixteen-Point Garbage Matte,Replicate,Noise HLS Auto,Noise HLS Auto,Channel Mixer,Motion,MXF/ARRIRAW Development Settings,Alpha Glow,Cineon Converter,Posterize,Transform,Transform,Offset,Canon Cinema RAW Light Source Settings,Fast Color Corrector,ASC CDL,Text,Lightning,Gaussian Blur,Noise HLS,Noise HLS,Color Emboss,ProcAmp,Video Limiter,Black & White,Change Color,Calculations,Remove Matte,Emboss,Grid,Cineon Source Settings,Crop,VR Color Gradients,Circle,Mask2,RGB Color Corrector,RED Source Settings,Find Edges,Tint,Cell Pattern,VR De-Noise,Leave Color,Opacity,Posterize Time,Video Limiter (legacy),Lens Flare,SDR Conform,Eyedropper Fill,Luma Key,Sony RAW MXF Source Settings,Shadow/Highlight,Mask,Sharpen,Brightness & Contrast,Metadata & Timecode Burn-in,Corner Pin,Ellipse,Basic 3D,Drop Shadow,Horizontal Flip,Mosaic,4-Color Gradient,Timecode,Threshold,Threshold,Write-on,Four-Point Garbage Matte,VR Fractal Noise,Strobe Light,Radial Shadow,VR Sharpen,VR Digital Glitch,Color Key,Color Balance,Brush Strokes,CinemaDNG Source Settings,Warp Stabilizer,VR Chromatic Aberrations,Channel Blur,VR Projection,Color Space Transform,Solid Composite,Luma Curve,Dust & Scratches,Simple Text,Track Matte Key,Image Matte Key,Noise Alpha,Noise Alpha,Auto Reframe,Group,Compound Arithmetic,Edge Feather,Invert,ARRIRAW Development Settings,Blend,Texturize".split(',').sort();
+        updateCounter(1)
+        e.preventDefault();
+        const placeholder = document.querySelector('.exclusions-placeholder');
+        if(placeholder) {
+            placeholder.remove();
+        }
+
+        let node = document.createElement("div");
+        let rmBtn = document.createElement('button');
+        let selectElem = document.createElement('select');
+
+        
+        rmBtn.classList.add('remove-exclusion');
+        rmBtn.innerHTML = 'x';
+
+        node.classList.add('exclusion')
+
+        let selectOption = document.createElement("option");
+        selectOption.value = null;  // Set the value attribute
+        selectOption.textContent = 'Select effect...';  // Set the visible text
+        selectElem.appendChild(selectOption);  // Add the option to the select element
+        selectElem.classList.add('exclusion-dropdown')
+
+        effectsList.forEach((effectOption) => {
+            let optionElement = document.createElement("option");
+            optionElement.value = effectOption;  // Set the value attribute
+            optionElement.textContent = effectOption;  // Set the visible text
+            selectElem.appendChild(optionElement);  // Add the option to the select element
+        })
+        
+        rmBtn.addEventListener('click', e => {
+            e.preventDefault();
+            e.srcElement.parentElement.remove();
+            updateCounter(-1);
+            togglePlaceholder()
+        })
+
+        node.appendChild(selectElem)
+        node.appendChild(rmBtn)
+
+        exclusionsContainer.appendChild(node);
+    })
+
+
+
+
+    togglePlaceholder()
 /*
     $("#getseqname").on("click", function(e){
             e.preventDefault(); 
