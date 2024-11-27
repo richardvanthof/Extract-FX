c014e83dc5191e62e60cc1ab9dfe45f6d055264c
Richard van 't Hof <hello@therichard.space>
2024-11-27 14:49:37 +0100

    Added Exclusion obj type + test data

--- src/js/global-vars/extract.ts
+++ src/js/global-vars/extract.ts
@@ -2,9 +2,14 @@ import { writable } from 'svelte/store';
 import type { Writable } from "svelte/store"
 import {defaultEffects, defaultTrackAmount} from './shared';
 
+type Exclusion = {
+    id: string,
+    effect: string
+}
+
 const sourceTrack:Writable<number> = writable(1);
 const destination:Writable<'file' | 'track'> = writable('file');
-const exclusions:Writable<string[]> = writable([]);
+const exclusions:Writable<Exclusion[]> = writable([{id: '1', effect: 'Color Replace'}]);
 const exclusionOptions:Writable<string[]> = writable(defaultEffects)
 const isExclusionModalOpen:Writable<boolean> = writable(true);
 
